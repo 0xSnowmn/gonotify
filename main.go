9a45ceb734c6be47868e33767a1a1b78cbdb6c6c
Youssef Ahmed <36892827+yghonem14@users.noreply.github.com>
2022-03-09 15:06:42 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,35 +7,44 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"sync"
 
 	flags "github.com/jessevdk/go-flags"
-	env "github.com/joho/godotenv"
+	_ "github.com/joho/godotenv"
 )
 
 // Setting Options
 var Options struct {
-	// File For urls
-	Data string `short:"d" long:"data" description:"data sends to channel" required:"false"`
+	// Data that will send
+	Data    string `short:"d" long:"data" description:"data sends to channel" required:"false"`
+	Channel string `short:"w" long:"chan" description:"channel" required:"false"`
 	// Concurrency For the requests
 	Concurrency int `short:"c" long:"concurrency" default:"25" description:"Concurrency For Requests"`
 }
 
 func main() {
 	var wg sync.WaitGroup
+	var hook_url string
+	EnvVars := make(map[string]string)
 	_, err := flags.Parse(&Options)
 	if err != nil {
 		return
 	}
-	err = env.Load()
+	env(EnvVars)
 	if err != nil {
-		fmt.Println("Error in Loading Env File")
-		return
+		panic(err)
 	}
 	// get the value of flags and get webhook url from .env file
 	data := Options.Data
 	conc := Options.Concurrency
-	hook_url := os.Getenv("SLACK_WEBHOOK")
+	channel := Options.Channel
+	if channel == "" {
+		hook_url = EnvVars["SLACK_WEBHOOK"]
+	} else {
+		hook_url = EnvVars["SLACK_WEBHOOK_"+channel]
+	}
 
 	// Check if user will send message with --data flag or via 'cat file'
 	if data == "" {
@@ -73,6 +82,25 @@ func getUrls() <-chan string {
 		}
 	}()
 	return urls
+}
+
+// Read urls from std input in case user do 'cat file'
+func env(vars map[string]string) {
+	// create env map
+	dirname, _ := os.UserHomeDir()
+	file, err := os.Open(filepath.Join(dirname, ".env"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	// scan all std
+	scan := bufio.NewScanner(file)
+	defer file.Close()
+	for scan.Scan() {
+		line := scan.Text()
+		// send every line to urls channel
+		nw := strings.Split(line, "=")
+		vars[nw[0]] = nw[1]
+	}
 }
 
 func sendMessage(hook_url string, message string) {
